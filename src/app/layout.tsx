69fa8d62d5396998eef1f69c48bcbf15d9e19a05
erikseki <erikseki@hotmail.com>
2025-03-29 23:31:50 -0300

    criando o component e testando o tailwind css

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,11 +10,12 @@ export const metadata: Metadata = {
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode;
+  children: React.ReactNode; 
 }>) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className="bg-black text-white">{children}</body>
     </html>
   );
 }
+
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
+import { Button } from "@/components/button";
+
 export default function Home() {
   return (
     <main>
-      <div>Hello world!</div>
+      <div className="text-white">Hello world!</div>
+      <Button/>
     </main>
   );
 }
