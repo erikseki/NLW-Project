32673beaa6b8f907bb0fe11961bf5dbcc28a2543
erikseki <erikseki@hotmail.com>
2025-04-10 23:54:16 -0300

    Updating the Input Component

--- src/components/input.tsx
+++ src/components/input.tsx
@@ -11,9 +11,9 @@ export function Input({error = false, ...props}: InputProps) {
   return (
     <div
      data-error={error}
-    className="group bg-gray-800 h-12 border border-gray-600 rounded-xl px-4 flex items-center gap-2 focus-within:border-gray-100"
+    className="group bg-gray-800 h-12 border border-gray-600 rounded-xl px-4 flex items-center gap-2 focus-within:border-gray-100 data-[error=true]:border-danger"
     >
-      <span className="text-gray-400 group-focus-within:text-gray-100 group-[&:not(:has(input:placeholder-shown))]:text-gray-100"
+      <span className="text-gray-400 group-focus-within:text-gray-100 group-[&:not(:has(input:placeholder-shown))]:text-gray-100 group-data-[error=true]:text-danger"
       >
         <Mail />
       </span>
