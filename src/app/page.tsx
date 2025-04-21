d20b3016c608e978a496946bbad2a4f561f56d1f
erikseki <erikseki@hotmail.com>
2025-04-20 23:53:27 -0300

    Set up folders and structure for home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,37 +1,3 @@
-import { Button } from "@/components/button";
-import { IconButton } from "@/components/icon-button";
-import { InputField, InputIcon, InputRoot } from "@/components/input";
-import { ArrowRight, Copy, Mail } from 'lucide-react'
-
 export default function Home() {
-  return (
-    <main>
-      {/* <div className="text-white">Hello world!</div>
-
-      <h1 className="text-4xl font-semibold font-heading">Hello World</h1>
-      
-      <Button text="Erik" />
-      <Button text="Seki" />
-      <Button text="" /> */}
-
-        <Button type="submit">
-            Enviar
-          <ArrowRight />
-        </Button>
-
-        <IconButton>
-          <Copy />
-        </IconButton>
-
-        <div>
-          <InputRoot>
-              <InputIcon>
-                <Mail className="size-5"/>
-              </InputIcon>
-
-              <InputField />
-          </InputRoot>
-        </div>
-    </main>
-  );
+  return <main className="max-w-[1240px] mx-auto px-5 py-8 md:py-0">asd</main>
 }
