35c71ecf34b6449a86a2e59d9aebd9e8ec11ce75
erikseki <erikseki@hotmail.com>
2025-04-26 23:34:50 -0300

    Adding the first elements to the homepage.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,9 +5,14 @@ import logo from '../assets/logo.png'
 export default function Home() {
   return (
     <main className="max-w-[1240px] mx-auto px-5 py-8 md:py-0">
-      <div className="h-dvh flex items-center justify-between gap-16 flex-col md:flex-row">
-        <div className="flex flex-col gap-10 w-full max-w-[550px]">
+      <div className="h-dvh flex justify-center gap-16 flex-col md:flex-row">
+        <div className="flex flex-col gap-8 items-center md:items-start">
           <Image src={logo} alt="devstage" width={108.5} height={30} />
+
+          <h1 className=" text-4xl text-center leading-none font-heading font-medium flex flex-col md:text-7xl md:text-left">
+            <span>CodeCraft</span>
+            Summit 2025
+          </h1>
         </div>
       </div>
     </main>
